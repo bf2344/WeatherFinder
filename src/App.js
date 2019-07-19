798223a0e3ca2d550c956fa83e176a30c64f2fc2
Brian <bford2344@gmail.com>
2019-07-19 14:29:56 -0600

    first component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Titles from './components/Titles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  //JSX in render method
+  render(){
+    return(
+    <div>
+      <Titles />
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
