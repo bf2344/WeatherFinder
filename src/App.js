783c9b34220d8d65451951789122746fa08de648
Brian <bford2344@gmail.com>
2019-07-19 15:27:37 -0600

    checkpoint

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ import Weather from './components/Weather'
 const API_KEY = "01b3cd1976a0c8efe2a1cf86798399b9"
 
 class App extends React.Component {
+state = {
+  temperature: undefined,
+  city: undefined,
+  country: undefined,
+  humidity: undefined,
+  description: undefined,
+  error: undefined
+  }
 
-getWeather = async () => {
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}
+getWeather = async (e) => {
+  e.preventDefault()
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value
+
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}id=524901&APPID=${API_KEY}
   `)
   const data = await api_call.json();
+  console.log(data)
+  this.setState({
+    temperature: data.main.temp,
+    city: data.name,
+    country: data.sys.country,
+    humidity: data.name.humidity,
+    description: data.weather[0].description,
+    error: ""
+  })
 }
 
   //JSX in render method
@@ -18,7 +39,7 @@ getWeather = async () => {
     return(
     <div>
       <Titles />
-      <Form />
+      <Form getWeather={this.getWeather}/>
       <Weather />
     </div>
     )
